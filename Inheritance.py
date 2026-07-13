3da4fbec4078d3572ce83d992a628df3b3a7f989
yehoshua <y0587828030@gmail.com>
2026-07-13 12:21:26 +0300

    2. Runner with a Fixed Sport

--- Inheritance.py
+++ Inheritance.py
@@ -16,3 +16,25 @@ class Swimmer(Athlete):
 
 b = Swimmer("tom", 22)
 b.introduce()
+
+
+## step 2 
+class Athlete:
+    def __init__(self, name, age,sport):
+        self.name = name 
+        self.age = age
+        self.sport = sport
+
+    def describe(self):
+        print(f"{self.name} competes in {self.sport}")
+a=Athlete("bob", 25, "Dance")
+a.describe()
+
+class Runner(Athlete):
+    def __init__(self, name, age):
+        super().__init__(name, age, "Running")
+
+b=Runner("sara", 25)
+b.describe()
+
+        
